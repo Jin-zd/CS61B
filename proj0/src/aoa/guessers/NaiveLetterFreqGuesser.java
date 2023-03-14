19567154ef86567798ddbb33145a3f28dc07a4d9
liquorice1548 <2295508804@qq.com>
2023-03-14 14:58:07 +0800

    v1

--- proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
+++ proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
@@ -1,8 +1,12 @@
 package aoa.guessers;
 
 import aoa.utils.FileUtils;
+import edu.princeton.cs.algs4.In;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class NaiveLetterFreqGuesser implements Guesser {
     private final List<String> words;
@@ -17,18 +21,48 @@ public class NaiveLetterFreqGuesser implements Guesser {
         return getGuess(guesses);
     }
 
-    /** Returns a map from a given letter to its frequency across all words.
-     *  This task is similar to something you did in hw0b! */
+    /**
+     * Returns a map from a given letter to its frequency across all words.
+     * This task is similar to something you did in hw0b!
+     */
     public Map<Character, Integer> getFrequencyMap() {
-        // TODO: Fill in this method.
-        return null;
+        Map<Character, Integer> countMap = new TreeMap<>();
+        for (String word : words) {
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                if (!countMap.containsKey(ch)) {
+                    countMap.put(ch, 1);
+                } else {
+                    countMap.put(ch, countMap.get(ch) + 1);
+                }
+            }
+        }
+        return countMap;
     }
 
-    /** Returns the most common letter in WORDS that has not yet been guessed
-     *  (and therefore isn't present in GUESSES). */
+    /**
+     * Returns the most common letter in WORDS that has not yet been guessed
+     * (and therefore isn't present in GUESSES).
+     */
     public char getGuess(List<Character> guesses) {
-        // TODO: Fill in this method.
-        return '?';
+        Map<Character, Integer> countMap;
+        countMap = getFrequencyMap();
+        if (countMap.isEmpty()) {
+            return '?';
+        }
+        for (char guess : guesses) {
+            countMap.remove(guess);
+        }
+        char firstKey = countMap.keySet().iterator().next();
+        int m = countMap.get(firstKey);
+        char ch = firstKey;
+        for (char key : countMap.keySet()) {
+            if (countMap.get(key) > m) {
+                m = countMap.get(key);
+                ch = key;
+            }
+        }
+        return ch;
     }
 
     public static void main(String[] args) {
