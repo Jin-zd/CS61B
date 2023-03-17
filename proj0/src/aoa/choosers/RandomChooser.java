2e883fcf854b2af2a9af80410e7ce66e61f51b06
liquorice1548 <2295508804@qq.com>
2023-03-17 17:28:15 +0800

    v7 完成RandomChooser类

--- proj0/src/aoa/choosers/RandomChooser.java
+++ proj0/src/aoa/choosers/RandomChooser.java
@@ -1,32 +1,76 @@
 package aoa.choosers;
 
 import edu.princeton.cs.algs4.StdRandom;
-import aoa.utils.FileUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RandomChooser implements Chooser {
     private final String chosenWord;
     private String pattern;
 
     public RandomChooser(int wordLength, String dictionaryFile) {
-        // TODO: Fill in/change this constructor.
-        chosenWord = "";
+        List<String> wordList = new ArrayList<>();
+        List<String> sameLengthWords = new ArrayList<>();
+
+        if (wordLength < 1) {
+            throw new IllegalArgumentException("error!");
+        }
+        try {
+            wordList = Files.readAllLines(Paths.get(dictionaryFile));
+        } catch (IOException er) {
+            er.printStackTrace();
+        }
+
+        for (String word : wordList) {
+            if (word.length() == wordLength) {
+                sameLengthWords.add(word);
+            }
+        }
+
+        if (sameLengthWords.isEmpty()) {
+            throw new IllegalStateException("error!");
+        }
+
+        int numWords = sameLengthWords.size();
+        int randInt = StdRandom.uniform(numWords);
+        chosenWord = sameLengthWords.get(randInt);
+
+        pattern = "";
+        for (int i = 0; i < wordLength; i++) {
+            pattern += "-";
+        }
     }
 
     @Override
     public int makeGuess(char letter) {
-        // TODO: Fill in this method.
-        return 0;
+        int count = 0;
+        char[] patternArray = new char[chosenWord.length()];
+        String temPattern = "";
+        for (int i = 0; i < pattern.length(); i++) {
+            patternArray[i] = pattern.charAt(i);
+        }
+        for (int i = 0; i < chosenWord.length(); i++) {
+            if (chosenWord.charAt(i) == letter) {
+                count++;
+                patternArray[i] = letter;
+            }
+            temPattern += patternArray[i];
+        }
+        pattern = temPattern;
+        return count;
     }
 
     @Override
     public String getPattern() {
-        // TODO: Fill in this method.
-        return "";
+        return pattern;
     }
 
     @Override
     public String getWord() {
-        // TODO: Fill in this method.
-        return "";
+        return chosenWord;
     }
 }
