d8da20f5211e5fe22f33d6b0b6f7705c3eb6f4bf
liquorice1548 <2295508804@qq.com>
2023-03-21 21:08:04 +0800

    v1

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -9,15 +9,23 @@ public class BombMain {
             phase = Integer.parseInt(args[0]);
         }
         // TODO: Find the correct passwords to each phase using debugging techniques
+        String password = "";
+        for (int i = 0; i < 100000; i++) {
+            if (i == 1337) {
+                password += "-81201430 ";
+            } else {
+                password += "1 ";
+            }
+        }
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            b.phase1(IntList.of(0, 9, 3, 0, 8)); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            b.phase2(password);
         }
     }
 }
