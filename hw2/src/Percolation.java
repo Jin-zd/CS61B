e4ee67820fe87df43b6238a47f22c8919c4e4488
liquorice1548 <2295508804@qq.com>
2023-04-14 20:19:48 +0800

    完成

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -1,38 +1,79 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-// TODO: Add any other necessary imports.
 
 public class Percolation {
-    // TODO: Add any necessary instance variables.
+    private final int[][] grid;
+    private final WeightedQuickUnionUF connectSet;
+    private int openCount;
+    private final int gridLength;
 
     public Percolation(int N) {
-        // TODO: Fill in this constructor.
+        if (N < 0) {
+            throw new IllegalArgumentException();
+        }
+        connectSet = new WeightedQuickUnionUF(N * N);
+        grid = new int[N+1][N+1];
+        openCount = 0;
+        gridLength = N;
+    }
+
+    private int correspondedIndex(int row, int col) {
+        return row * gridLength + col;
+    }
+
+    private void connectItem(int row, int col, int currIndex) {
+        if (row < 0 || row >= gridLength || col < 0 || col >= gridLength) {
+            return;
+        }
+        int index = correspondedIndex(row, col);
+        connectSet.union(currIndex, index);
     }
 
     public void open(int row, int col) {
-        // TODO: Fill in this method.
+        if (row < 0 || row >= gridLength || col < 0 || col >= gridLength) {
+            throw new IndexOutOfBoundsException();
+        }
+        grid[row][col] = 1;
+        openCount += 1;
+        int currIndex = correspondedIndex(row, col);
+        connectItem(row - 1, col, currIndex);
+        connectItem(row + 1, col, currIndex);
+        connectItem(row, col - 1, currIndex);
+        connectItem(row, col + 1, currIndex);
     }
 
     public boolean isOpen(int row, int col) {
-        // TODO: Fill in this method.
-        return false;
+        if (row < 0 || row >= gridLength || col < 0 || col >= gridLength) {
+            throw new IndexOutOfBoundsException();
+        }
+        return grid[row][col] == 1;
     }
 
     public boolean isFull(int row, int col) {
-        // TODO: Fill in this method.
+        if (row < 0 || row >= gridLength || col < 0 || col >= gridLength) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (!isOpen(row, col)) {
+            return false;
+        }
+        int currIndex = correspondedIndex(row, col);
+        for (int i = 0; i < gridLength; i++) {
+            if (connectSet.connected(currIndex, i)) {
+                return true;
+            }
+        }
         return false;
     }
 
     public int numberOfOpenSites() {
-        // TODO: Fill in this method.
-        return 0;
+        return openCount;
     }
 
     public boolean percolates() {
-        // TODO: Fill in this method.
+        for (int i = 0; i < gridLength; i++) {
+            if (isFull(gridLength, i)) {
+                return true;
+            }
+        }
         return false;
     }
-
-    // TODO: Add any useful helper methods (we highly recommend this!).
-    // TODO: Remove all TODO comments before submitting.
-
 }
